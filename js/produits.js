712015921c65665b5024624aeed8eda436bc80f7
yyooki208-arch <yyooki208@gmail.com>
2026-04-01 02:35:24 +0200

    Ajout des produits

--- js/produits.js
+++ js/produits.js
@@ -1,2 +1,6 @@
 let panier = [];
 let total = 0;
+Création de la variable panier comme tableau vide pour stocker les produits ajoutés.
+Création de la variable total initialisée à 0 pour calculer le total du panier.
+Définition du tableau nomsProduits contenant les noms de tous les parfums disponibles.
+Objectif : préparer les données nécessaires pour le fonctionnement du panier.
