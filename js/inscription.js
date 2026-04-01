15152f7ab3ce728c0d3d65443e5ac37ff4ffb0fd
kheireddinehadrich26-maker <kheireddinehadrich26@gmail.com>
2026-04-01 01:01:10 +0100

    init: create base HTML structure

---
+++ js/inscription.js
@@ -0,0 +1,23 @@
+function validerFormulaire() {
+  var nom = document.getElementById("nom");
+  var prenom = document.getElementById("prenom");
+  var email = document.getElementById("email");
+
+  var message = "";
+
+  if (nom.value === "") message += "- Le nom est obligatoire.\n";
+  if (prenom.value === "") message += "- Le prénom est obligatoire.\n";
+
+  if (email.value === "") {
+    message += "- L'email est obligatoire.\n";
+  } else if (!email.value.includes("@")) {
+    message += "- Email invalide.\n";
+  
+
+  if (message !== "") {
+    alert(message);
+    return false;
+  }
+
+  return true;
+}
